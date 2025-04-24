5b8c72da7c7b108644c379c33337fc60645ee372
vnecnx <ernestkfowler@gmail.com>
2025-04-24 10:03:30 -0500

    All default pages done, need to make pages for portfolio pieces

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-// JS scripts placed here
+// SPOTIFY CURRENTLY PLAYING
+
+SCP({
+    selector: '#spotify-widget',
+    username: 'vnecnx',
+    api_key: '5bfeaab84ca3bfae2d6057df84002a7f',
+    width: '100%',
+    height: '370'
+});
